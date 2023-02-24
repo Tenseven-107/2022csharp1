94073d9d20da7bbf1caa6555019698d8abbcb129
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2023-02-24 12:12:02 +0100

    Did some assignments

---
+++ VariableOpdracht/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class TwitchStream
+    {
+        //maak hier class variables aan:
+        // type=string, naam: name,
+        // type=string, naam: lastStreamName
+        // type=Game, naam: streamGame
+        // type=int, naam: followers
+        // type=bool, naam: automod
+
+        string name = "Moist";
+        string lastStreamName = "state of play";
+        Game streamGame = new Game("Halo CE");
+        long  followers = 4800000000;
+        bool automod = true;
+    }
+}
