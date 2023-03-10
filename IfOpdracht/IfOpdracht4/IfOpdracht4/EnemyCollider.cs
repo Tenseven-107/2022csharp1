76645c87232f6e211e5c60b92b7fe2df138dbb22
Tenseven-107 <78176691+Tenseven-107@users.noreply.github.com>
2023-03-10 10:54:57 +0100

    completed ifelse, ez

---
+++ IfOpdracht/IfOpdracht4/IfOpdracht4/EnemyCollider.cs
@@ -0,0 +1,26 @@
+﻿namespace IfOpdracht4
+{
+    internal class EnemyCollider
+    {
+        internal bool CanPassWall(Enemy enemy)
+        {
+            //gebruik hier een || (OR)
+            //je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
+            if (enemy.flying || enemy.incoporial || enemy.subterranian)
+            {
+                return true;//vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian) true is;
+            }
+            else return false;
+        }
+        internal bool CanPassWater(Enemy enemy)
+        {
+            //gebruik hier een || (OR)
+            //je kan enemy.??? gebruiken om de eigenschap te testen, bv voor flying wordt het enemy.flying
+            if (enemy.flying || enemy.incoporial || enemy.subterranian)
+            {
+                return true;//vervang false, een enemy mag door een muur als 1 van  (flying,incoporial,subterranian,swimmer) true is;
+            }
+            else return false;
+        }
+    }
+}
